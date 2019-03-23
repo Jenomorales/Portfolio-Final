4048a30053a25cb21f32039d7a12216ace4c3ec7
Jenomorales <43798454+Jenomorales@users.noreply.github.com>
2019-03-23 14:08:42 -0700

    added lightbox

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,20 @@
 // Everything OK?
-console.log('OK');
-$(".thumbnail").fitVids();
 
+$('a').click( function( event ){
+
+    // Hijack the Click
+    event.preventDefault();
+  
+    console.log('OK');
+    // Show Lightbox
+    $('#lightbox').toggleClass('showLightbox');
+  
+});
+
+
+$('#lightbox').click(function(){
+  
+   $('#lightbox').toggleClass('showLightbox');
+      
+});
 // Your Awesome Scripts
